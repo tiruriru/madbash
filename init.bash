8935528284639581534dd8072143d53ac47643fc
Chris Kowalik <chris@nu7hat.ch>
2013-09-17 18:19:32 -0300

    default aliases moved to core plugin

--- init.bash
+++ init.bash
@@ -14,24 +14,6 @@ export PATH=/usr/local/bin:$PATH
 #-------------------------------------------------------------------------------
 source $MADBASH/themes/${THEME}.bash
 
-# Default aliases.
-#-------------------------------------------------------------------------------
-alias edit="$EDITOR"
-alias e='edit'
-alias ls='ls -G'
-alias la='ls -AF'
-alias ll='ls -al'
-alias l='ls -a'
-alias l1='ls -1'
-alias c='clear'
-alias q='exit'
-alias md='mkdir -p'
-alias rd='rmdir'
-alias h='history'
-alias ..='cd ..'
-alias ...='cd ../..'
-alias _='sudo'
-
 # Set some global helper constants.
 #-------------------------------------------------------------------------------
 OS=`uname -s`
---
+++ plugins/core.bash
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------byn
+# file: plugins/core.bash
+#
+# General aliases, plugins and extras.
+#-------------------------------------------------------------------------------
+
+# Custom aliases.
+#-------------------------------------------------------------------------------
+alias edit="$EDITOR"
+alias e='edit'
+alias ls='ls -G'
+alias la='ls -AF'
+alias ll='ls -al'
+alias l='ls -a'
+alias l1='ls -1'
+alias c='clear'
+alias q='exit'
+alias md='mkdir -p'
+alias rd='rmdir'
+alias h='history'
+alias ..='cd ..'
+alias ...='cd ../..'
+alias _='sudo'
