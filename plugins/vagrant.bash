ba3da02a7889bdbf4c1819ef1015f8b79f35407f
Chris Kowalik <chris@nu7hat.ch>
2013-09-18 00:56:34 -0300

    added vagrant aliases

---
+++ plugins/vagrant.bash
@@ -0,0 +1,16 @@
+#-------------------------------------------------------------------------------
+# file: plugins/vagrant.bash
+#
+# Vagrant aliases and extras.
+#-------------------------------------------------------------------------------
+
+# Custom aliases.
+#-------------------------------------------------------------------------------
+alias vup='vagrant up'
+alias vpr='vagrant provision'
+alias vsd='vagrant suspend'
+alias vre='vagrant resume'
+alias vrl='vagrant reload'
+alias vst='vagrant status'
+alias vsh='vagrant ssh'
+alias vrm='vagrant destroy'
